b5e114d0238df59da188e3158417e436aefe08b3
Anamul Haque <anamul.ice14@gmail.com>
2024-04-08 17:02:08 +0600

    Update mongodb url

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,9 @@
-const mongoose = require('mongoose')
-const asyncErrorHandler = require('./error/asyncErrorHandler')
+const mongoose = require("mongoose");
+const asyncErrorHandler = require("./error/asyncErrorHandler");
 
+const dbConnect = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URL);
+  console.log(`Mongodb is connected on ${conn.connection.host}`);
+};
 
-const dbConnect =  async() =>{
-    const conn = await mongoose.connect(process.env.MONGO_LOCAL)
-    console.log(`Mongodb is connected on ${conn.connection.host}`)
-}
-
-
-module.exports = dbConnect
+module.exports = dbConnect;
